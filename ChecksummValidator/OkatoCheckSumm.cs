7464e45fe2a647e96a652822f5dcf64569577a9f
bercut497 <bercut497@gmail.com>
2018-03-29 00:26:49 +0500

    sugar classes

---
+++ ChecksummValidator/OkatoCheckSumm.cs
@@ -0,0 +1,17 @@
+﻿using ChecksummValidator.Enums;
+
+namespace ChecksummValidator
+{
+    public static class OkatoCheckSumm
+    {
+        public static bool IsValid(object value)
+        {
+            return ClassificationCode.IsValid(value, ClassificationCodeType.okato);
+        }
+
+        public static bool IsValid(string value)
+        {
+            return ClassificationCode.IsValid(value, ClassificationCodeType.okato);
+        }
+    }
+}
---
+++ ChecksummValidator/OkpoCheckSumm.cs
@@ -0,0 +1,17 @@
+﻿using ChecksummValidator.Enums;
+
+namespace ChecksummValidator
+{
+    public static class OkpoCheckSumm
+    {
+        public static bool IsValid(object value)
+        {
+            return ClassificationCode.IsValid(value, ClassificationCodeType.okpo);
+        }
+
+        public static bool IsValid(string value)
+        {
+            return ClassificationCode.IsValid(value, ClassificationCodeType.okpo);
+        }
+    }
+}
