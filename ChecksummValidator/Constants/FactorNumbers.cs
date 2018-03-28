b6167119ffd943577486e249cde6f5b5c4825518
bercut497 <bercut497@gmail.com>
2018-03-29 00:24:34 +0500

    bank number checksumm constant

--- ChecksummValidator/Constants/FactorNumbers.cs
+++ ChecksummValidator/Constants/FactorNumbers.cs
@@ -2,7 +2,7 @@ namespace ChecksummValidator.Constants
 {
     internal static class FactorNumbers
     {
-
-        public static readonly byte[] InnFactorNumbers = {3,7,2,4,10,3,5,9,4,6,8};
-    } 
+        public static readonly byte[] InnFactorNumbers = { 3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8 };
+        public static readonly byte[] BankFactorNumbers = { 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1, 3, 7, 1 };
+    }
 }
